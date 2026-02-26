291ce5bff3f31da68f72d36a120676cd1842ada0
amycardona <amycardona07@gmail.com>
2026-02-25 17:02:47 -0700

    Add files via upload

---
+++ tree_height.py
@@ -0,0 +1,38 @@
+def tree_height(tree):
+    if tree is None:
+        return 0
+    left_height = tree_height(tree._left)
+    right_height = tree_height(tree._right)
+    return max(left_height, right_height) + \
+        (0 if tree._left is None and tree._right is None else 1)
+
+class BinaryTree:
+    def __init__(self,value):
+        self._value = value
+        self._left = None
+        self._right = None
+
+    def value(self):
+        return self._value
+
+    def left(self):
+        return self._left
+
+    def right(self):
+        return self._right
+
+    def insert_right(self, value):
+        if self._right == None:
+            self._right = BinaryTree(value)
+        else:
+            t = BinaryTree(value)
+            t._right = self._right
+            self._right = t
+
+    def insert_left(self, value):
+        if self._left == None:
+            self._left = BinaryTree(value)
+        else:
+            t = BinaryTree(value)
+            t._left = self._left
+            self._left = t
